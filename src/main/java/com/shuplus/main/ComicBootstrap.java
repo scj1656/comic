abaecf61ce6732d0bc8ff33071eb5c51291d30b0
xiaojie <scj1656@163.com>
2018-08-28 00:57:06 +0800

    first commit

---
+++ src/main/java/com/shuplus/main/ComicBootstrap.java
@@ -0,0 +1,17 @@
+package com.shuplus.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用引导程序
+ * @author xiaojie
+ *
+ */
+@SpringBootApplication
+public class ComicBootstrap {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ComicBootstrap.class, args);
+	}
+}
